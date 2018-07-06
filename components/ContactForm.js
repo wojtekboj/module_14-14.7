9e7cb4cddec937ef74d9c56b51bbf8a9fe5d0ed4
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-06 09:48:46 +0200

    Modification ContactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -4,13 +4,13 @@ var ContactForm = React.createClass({
     },
 
     render: function() {
-      return (
-        <form className={'contactForm'}>
-          <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}/>
-          <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}/>
-          <input type={'email'} placeholder={'Email'} value={this.props.contact.email}/>
-          <button type={'submit'}>Dodaj kontakt</button>
-        </form>
-      )
+        return (
+            <form className={'contactForm'}>
+                <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}/>
+                <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}/>
+                <input type={'email'} placeholder={'Email'} value={this.props.contact.email}/>
+                <button type={'submit'}>Dodaj kontakt</button>
+            </form>
+        )
     },
 })
