31545b34d0684443ccc6b2016a141186ad3d6b16
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-06 09:33:22 +0200

    Add project files

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+
+    render: function() {
+      return (
+          <div className={'contactItem'}>
+              <img className={'contactImage'} src={'http://icons.veryicon.com/256/Application/Long%20Shadow%20Media/Contact.png'}/>
+              <p className={'contactLabel'}>
+                  Imię: {this.props.item.firstName}
+              </p>
+              <p className={'contactLabel'}>
+                  Nazwisko: {this.props.item.firstName}
+              </p>
+              <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+                  {this.props.item.email}
+              </a>
+          </div>
+      )
+    },
+  });
---
+++ components/Contacts.js
@@ -0,0 +1,21 @@
+var Contacts = React.createClass({
+    propTypes: {
+        items: React.PropTypes.array.isRequired,
+    },
+
+    render: function() {
+      var contacts = this.props.items.map(function(contact) {
+      return (
+          <Contact item={contact} key={contact.id}/>
+      )
+          //return React.createElement(Contact, {item: contact, key: contact.id});
+      });
+  
+      return (
+          <ul className={'contactsList'}>
+            {contacts}
+          </ul>
+        //React.createElement('ul', {className: 'contactsList'}, contacts)
+      );
+    }
+  });
