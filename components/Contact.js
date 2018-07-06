648eaa0488a4aa971f731ef956b70a825febe73b
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-06 09:48:15 +0200

    Modification Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -4,19 +4,19 @@ var Contact = React.createClass({
     },
 
     render: function() {
-      return (
-          <div className={'contactItem'}>
-              <img className={'contactImage'} src={'http://icons.veryicon.com/256/Application/Long%20Shadow%20Media/Contact.png'}/>
-              <p className={'contactLabel'}>
-                  Imię: {this.props.item.firstName}
-              </p>
-              <p className={'contactLabel'}>
-                  Nazwisko: {this.props.item.lastName}
-              </p>
-              <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
-                  {this.props.item.email}
-              </a>
-          </div>
-      )
+        return (
+            <div className={'contactItem'}>
+                <img className={'contactImage'} src={'https://icons.veryicon.com/256/Application/Long%20Shadow%20Media/Contact.png'}/>
+                <p className={'contactLabel'}>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className={'contactLabel'}>
+                    Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
+        )
     },
-  });
+});
